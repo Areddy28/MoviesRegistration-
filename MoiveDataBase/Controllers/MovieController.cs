b55d8ab8d26e8c4f3aded3455cff673a59364151
areddy1 <AnuragReddy@rocketmortgage.com>
2021-12-13 09:03:34 -0500

    MOVIEDB commit

--- MoiveDataBase/Controllers/MovieController.cs
+++ MoiveDataBase/Controllers/MovieController.cs
@@ -6,53 +6,91 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace MoiveDataBase.Controllers
 {
     public class MovieController : Controller
     {
+        public MovieDAL MovieDB = new MovieDAL();
+
         public IActionResult Index()
         {
-            using (var connect = new MySqlConnection(Secret.Connection))
-            {
+            List<Movies> movies = MovieDB.GetMovies();
+            return View(movies);
+        }
 
-              string sql = "select * from movies";
+        public IActionResult Details(int id)
+        {
 
-            connect.Open();
+            Movies m = MovieDB.GetMovie(id);
+            return View(m);
+        }
 
-            List<Movies> movies = connect.Query<Movies>(sql).ToList();
-
-                connect.Close();
-
-                
-               Movies m = movies[0];
-               
-               return View(m);
-               
-            }
-
-
+        public IActionResult Delete(int id)
+        {
+            Movies m = MovieDB.GetMovie(id);
+            return View(m);
         }
 
 
-        public IActionResult Registration(Movies m)
+        public IActionResult DeleteFromDB(int id)
         {
-            
+            MovieDB.DeleteMovie(id);
+            return RedirectToAction("Index", "Home");
+        }
 
-                return View(m); 
-
-           
+        //This action will display the view as well as pass the movie
+        public IActionResult Edit(int id)
+        {
+            Movies m = MovieDB.GetMovie(id);
+            return View(m);
         }
 
         [HttpPost]
-        public IActionResult Index(Movies t)
-        {
-            return RedirectToAction("Result", t);
-        }
-        public IActionResult Result(Movies m)
+        public IActionResult Edit(Movies m)
         {
 
-            return RedirectToAction("Result", m);
+            MovieDB.UpdateMovie(m);
+            return RedirectToAction("Index", "Home");
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(Movies m)
+        {
+            //ModelState is valid checks the model against its data annotations
+            //it returns if all rules are met 
+            //it returns if any rule is violated
+            if (ModelState.IsValid)
+            {
+
+                MovieDB.CreateMovie(m);
+                return RedirectToAction("Index", "Home");
+
+            }
+            else
+            {
+                //If the model is bad we will return 
+                return View(m);
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    
     }
 }
---
+++ MoiveDataBase/Models/MovieDALcs.cs
@@ -0,0 +1,106 @@
+﻿using Dapper;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoiveDataBase.Models
+{
+    public class MovieDAL
+    {
+        //This will contain all of our code for talking to the DB, and can be reused anywhere in our app
+
+        //This has several methods, each one representing our CRUD
+        //C- Create
+        //R- Read (all, individual)
+        //U- Update (aka edit)
+        //D- Delete or Destroy
+
+
+        //Read all method 
+        public List<Movies> GetMovies()
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                var sql = "select * from movies";
+                connect.Open();
+                List<Movies> movies = connect.Query<Movies>(sql).ToList();
+                connect.Close();
+
+                return movies;
+            }
+
+        }
+
+        //Read single - take in an id and return the matching row
+        public Movies GetMovie(int id)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = "select * from movies where id=" + id;
+                connect.Open();
+                //Query always returns a list regardless of how many movies we want.
+                //Even if our query is mean to return 1 movie, we still need to pull it out of a list of count 1
+                Movies m = connect.Query<Movies>(sql).First();
+                connect.Close();
+
+                return m;
+            }
+        }
+
+        public void DeleteMovie(int id)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = "delete from Movies where id=" + id;
+                connect.Open();
+                //Query always returns a list regardless of how many movies we want.
+                //Even if our query is mean to return 1 movie, we still need to pull it out of a list of count 1
+                connect.Query<Movies>(sql);
+                connect.Close();
+
+
+            }
+
+        }
+
+        public void UpdateMovie(Movies m)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = "update movies " +
+                    $"set title='{m.Title}', genre='{m.Genre}', `year`={m.Year}, runtime={m.Runtime} " +
+                    $"where id={m.Id}";
+                connect.Open();
+                //Query always returns a list regardless of how many movies we want.
+                //Even if our query is mean to return 1 movie, we still need to pull it out of a list of count 1
+                connect.Query<Movies>(sql);
+                connect.Close();
+
+
+            }
+
+
+        }
+
+        public void CreateMovie(Movies m)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = "insert into movies " +
+                    $"values(0, '{m.Title}', '{m.Genre}', {m.Year}, {m.Runtime}) ";
+                connect.Open();
+                //Query always returns a list regardless of how many movies we want.
+                //Even if our query is mean to return 1 movie, we still need to pull it out of a list of count 1
+                connect.Query<Movies>(sql);
+                connect.Close();
+
+
+            }
+
+
+        }
+    }
+
+    }
